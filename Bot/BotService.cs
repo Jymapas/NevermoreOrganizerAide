e0dec0e21dcb1b1b59c7986e95c3f552ad8f788f
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-04-08 00:58:37 +0500

    feat(Bot): add BotService

---
+++ Bot/BotService.cs
@@ -0,0 +1,29 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace NevermoreOrganizerAide.Bot;
+
+public class BotService(string token)
+{
+    private readonly TelegramBotClient _botClient = new(token);
+    private readonly ReceiverOptions _receiverOptions = new();
+
+    public void Start(string token)
+    {
+        var cts = new CancellationTokenSource();
+        _botClient.StartReceiving(HandleUpdate, HandleError, _receiverOptions, cts.Token);
+    }
+
+    private Task HandleUpdate(ITelegramBotClient bot, Update update, CancellationToken ct)
+    {
+        Console.WriteLine("Not implemented");
+        return Task.CompletedTask;
+    }
+
+    private Task HandleError(ITelegramBotClient bot, Exception ex, CancellationToken ct)
+    {
+        Console.WriteLine($"Ошибка бота: {ex.Message}");
+        return Task.CompletedTask;
+    }
+}
