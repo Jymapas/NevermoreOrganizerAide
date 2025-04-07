b963e922f4c105c6ef487e192ddfa019cf18337e
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-04-07 13:53:18 +0500

    chore: check flow of adding text to image

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Processing;
+
+var image = Image.Load("image.png");
+const string text = "Приём заявок";
+var font = SystemFonts.CreateFont("Arial", 48);
+var colour = Color.Black;
+var location = new PointF(50, 50);
+
+image.Mutate(x => x.DrawText(text, font, colour, location));
+
+image.Save("result.png");
