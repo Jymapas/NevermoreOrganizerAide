a871e0bf3779e636e1b69a9e38aa5863fb9d0375
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-04-08 00:07:18 +0500

    feat(ImageProcessing): create AddTextToImage method

---
+++ ImageProcessing/ImageService.cs
@@ -0,0 +1,21 @@
+﻿using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Processing;
+
+namespace NevermoreOrganizerAide.ImageProcessing;
+
+public class ImageService
+{
+    public void AddTextToImage()
+    {
+        var image = Image.Load("image.png");
+        const string text = "Регистрация";
+        var font = SystemFonts.CreateFont("Arial", 48);
+        var colour = Color.Black;
+        var location = new PointF(50, 50);
+    
+        image.Mutate(x => x.DrawText(text, font, colour, location));
+        image.Save("result.png");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,4 @@
-﻿using SixLabors.Fonts;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Drawing.Processing;
-using SixLabors.ImageSharp.Processing;
+﻿using NevermoreOrganizerAide.ImageProcessing;
 
-var image = Image.Load("image.png");
-const string text = "Приём заявок";
-var font = SystemFonts.CreateFont("Arial", 48);
-var colour = Color.Black;
-var location = new PointF(50, 50);
-
-image.Mutate(x => x.DrawText(text, font, colour, location));
-
-image.Save("result.png");
+var imageService = new ImageService();
+imageService.AddTextToImage();
